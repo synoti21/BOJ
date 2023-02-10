5074524d36712d82f2e863d7bb6d5cf2aae4483a
Jason <synoti21@naver.com>
2023-02-10 18:11:14 +0900

    7579 - First Attempt

--- Graph/7579.py
+++ Graph/7579.py
@@ -1,13 +1,12 @@
 from collections import deque
-d = [(0,1),(0,-1),(1,0),(-1,0)]
+d = [(0,0,1),(0,0,-1),(0,1,0),(0,-1,0),(1,0,0),(-1,0,0)] #z,y,x
 
 M,N,T = map(int, input().split())
 graph = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(T)]
 t_cnt = 0
+rt_cnt = 0
 day = 0
 rt_list = []
-
-print(graph)
 
 for i in range(T):
     for j in range(N):
@@ -17,4 +16,26 @@ for i in range(T):
                 t_cnt += 1
             if graph[i][j][k] == 1:
                 rt_list.append([i, j, k])
-print(rt_list)
+
+queue = deque()
+for i in rt_list:
+    rt_cnt += 1
+    queue.append(i)
+
+while queue:
+    x = queue.popleft()
+    day = graph[x[0]][x[1]][x[2]]
+    for i in d:
+        dx = x[2]+i[2]
+        dy = x[1]+i[1]
+        dz = x[0]+i[0]
+        if 0 <= dx < M and 0 <= dy < N and 0 <= dz < T and graph[dz][dy][dx] == 0:
+            graph[dz][dy][dx] = graph[x[0]][x[1]][x[2]] + 1
+            queue.append((dz,dy,dx))
+            rt_cnt += 1
+
+if rt_cnt == t_cnt:
+    print(day-1)
+else:
+    print(-1)
+
