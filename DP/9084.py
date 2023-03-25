2b954607fbabbab05e5696ab996ca13dcd1346de
Jason <synoti21@naver.com>
2023-03-26 02:21:49 +0900

    1149_top down

---
+++ DP/9084.py
@@ -0,0 +1,13 @@
+t = int(input())
+def sol():
+    n = int(input())
+    coin = list(map(int, input().split()))
+    m = int(input())
+    dp = [0 for _ in range(m)]
+
+
+
+
+
+for _ in range(t):
+    sol()
