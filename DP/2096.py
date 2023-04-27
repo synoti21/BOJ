9578846254b1e9d366d57768248e95d39fe93272
Jason <synoti21@naver.com>
2023-04-27 16:18:01 +0900

    2096

--- DP/2096.py
+++ DP/2096.py
@@ -1,24 +1,33 @@
 n = int(input())
 arr = [0 for _ in range(3)]
 dp_max = [0 for _ in range(3)]
+prev_dp_max = [0 for _ in range(3)]
+
 dp_min = [0 for _ in range(3)]
+prev_dp_min = [0 for _ in range(3)]
+
 ans = []
 
 arr = list(map(int, input().split()))
 for i in range(3):
     dp_max[i] = arr[i]
     dp_min[i] = arr[i]
+    prev_dp_max[i] = arr[i]
+    prev_dp_min[i] = arr[i]
 
 for i in range(1,n):
     arr = list(map(int, input().split()))
-    dp_max[0] = dp_max[0] + max(arr[0], arr[1])
-    dp_max[1] = dp_max[1] + max(arr[0], arr[1], arr[2])
-    dp_max[2] = dp_max[2] + max(arr[1], arr[2])
+    dp_max[0] = max(prev_dp_max[0]+arr[0], prev_dp_max[1]+arr[0])
+    dp_max[1] = max(prev_dp_max[0]+arr[1], prev_dp_max[1]+arr[1], prev_dp_max[2]+arr[1])
+    dp_max[2] = max(prev_dp_max[1]+arr[2], prev_dp_max[2]+arr[2])
 
-    dp_min[0] = dp_min[0] + min(arr[0], arr[1])
-    dp_min[1] = dp_min[1] + min(arr[0], arr[1], arr[2])
-    dp_min[2] = dp_min[2] + min(arr[1], arr[2])
-    print(dp_max)
-    print(dp_min)
+    dp_min[0] = min(prev_dp_min[0] + arr[0], prev_dp_min[1] + arr[0])
+    dp_min[1] = min(prev_dp_min[0] + arr[1], prev_dp_min[1] + arr[1], prev_dp_min[2] + arr[1])
+    dp_min[2] = min(prev_dp_min[1] + arr[2], prev_dp_min[2] + arr[2])
+
+    for i in range(3):
+        prev_dp_max[i] = dp_max[i]
+        prev_dp_min[i] = dp_min[i]
+
 
 print(str(sorted(dp_max,reverse=True)[0]) + " " + str(sorted(dp_min)[0]))
