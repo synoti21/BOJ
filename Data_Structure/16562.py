18095b4887c82bf8164de5474ace3a8fca2935d8
Jason <synoti21@naver.com>
2023-05-22 17:40:51 +0900

    16562

---
+++ Data_Structure/16562.py
@@ -0,0 +1,48 @@
+import sys
+
+INF = sys.maxsize
+n,m,k = map(int,input().split())
+f_cost = list(map(int,input().split()))
+f_cost.insert(0,0)
+parent = [[i,f_cost[i]] for i in range(n+1)]
+
+def find(a):
+    if parent[a][0] == a:
+        return a
+    p = find(parent[a][0])
+    parent[a][0] = p
+    return parent[a][0]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+
+    cost_a = parent[a][1]
+    cost_b = parent[b][1]
+
+    min_cost = min(cost_a, cost_b)
+    if a == b:
+        return
+    else:
+        parent[a][0] = b
+        parent[a][1] = min_cost
+        parent[b][1] = min_cost
+
+for _ in range(m):
+    v,w = map(int,input().split())
+    union(v,w)
+
+
+ans = []
+sum_cost = 0
+for i in range(len(parent)):
+    p = find(parent[i][0])
+
+    if p not in ans:
+        sum_cost += parent[p][1]
+        ans.append(parent[p][0])
+
+if k >= sum_cost:
+    print(sum_cost)
+else:
+    print("Oh no")
